c73337026d1f5095effcaecb0cdb90628a3d2df3
TunnelSight <167070589+TunnelSight@users.noreply.github.com>
2024-05-17 15:00:15 +0900

    Update UIManager.cs
    //매개변수: 오브젝트
    OpenPopup
    ClosePopup
    
    //UI매니저에 등록된 오브젝트 4개 켜기/끄기
    //매개변수:bool isActive
    SetIntroUI
    SetPlayUI
    SetSelectCharUI
    SetGameOveroUI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] GameObject IntroUI; //인트로창
+    [SerializeField] GameObject PlayUI; //게임 플레이창
+    [SerializeField] GameObject SelectCharUI; //캐릭터 선택창
+    [SerializeField] GameObject GameOver; //게임오버창
+
     public static UIManager Instance { get; private set; }
 
     private void Awake()
@@ -28,29 +33,35 @@ public class UIManager : MonoBehaviour
 
     void Update()
     {
-        
+
     }
 
-    void GameStart()
-    {
-        SceneManager.LoadScene("MainScene");
-
-        Debug.Log("게임 시작됨");
-    }
-
-    void GotoTitle()
-    {
-        SceneManager.LoadScene("IntroScene");
-
-        Debug.Log("메인화면으로 이동");
-    }
-
-    public void OpenPopup(GameObject gameObject)
+    public void OpenPopup(GameObject gameObject) //팝업창 열기
     {
         gameObject.SetActive(true);
     }
-    public void ClosePopup(GameObject gameObject)
+    public void ClosePopup(GameObject gameObject) //팝업창 닫기
     {
         gameObject.SetActive(false);
+    }
+
+    public void SetIntroUI(bool isActive) //인트로창
+    {
+        IntroUI.gameObject.SetActive(isActive);
+    }
+
+    public void SetPlayUI(bool isActive) //플레이창
+    {
+        PlayUI.gameObject.SetActive(isActive);
+    }
+
+    public void SetSelectCharUI(bool isActive) //캐릭터선택창
+    {
+        SelectCharUI.gameObject.SetActive(isActive);
+    }
+
+    public void SetGameOveroUI(bool isActive) //게임오버창
+    {
+        GameOver.gameObject.SetActive(isActive);
     }
 }
