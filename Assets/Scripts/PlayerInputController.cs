21b972ddc521f77367da1223628b1182be3e23e0
ksoo020406 <ksoo020406@gmail.com>
2024-05-19 10:11:22 +0900

    [Improve]LateUpdate() 안의 내용을 OnMove()로 이동

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -10,21 +10,16 @@ public class PlayerInputController : CharacterMoveController
 {
     [SerializeField] private SpriteRenderer characterSpriteRenderer; // 캐릭터 스프라이트 조작
 
-    private Vector2 inputValue; // OnMove와 함께 LateUpdate에서도 사용하기 위해 CallMoveEvent로 들어가게 되는 백터 값을 전역변수로 설정
-
     public void OnMove(InputValue value) // InputValue value는 유저가 입력한 값을 저장
     {
-        inputValue = value.Get<Vector2>().normalized; // .normalized 무조건 길이가 범위 안으로
-        CallMoveEvent(inputValue); // CharacterMoveController를 참조해 사용
-    }
+        Vector2 moveInput = value.Get<Vector2>().normalized; // .normalized 무조건 길이가 범위 안으로
+        CallMoveEvent(moveInput); // CharacterMoveController를 참조해 사용
 
-    private void LateUpdate()
-    {
-        if (inputValue.x > 0)
+        if (moveInput.x > 0)
         {
             characterSpriteRenderer.flipX = false;
         }
-        else if (inputValue.x < 0)
+        else if (moveInput.x < 0)
         {
             characterSpriteRenderer.flipX = true;
         }
