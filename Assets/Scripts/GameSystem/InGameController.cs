2b8fab6abc8a2cdea2b46e99f20364e92b32171d
bj961 <73685098+bj961@users.noreply.github.com>
2024-05-22 11:25:41 +0900

    최고 점수 저장 추가 / 테스트씬 수정

--- Assets/Scripts/GameSystem/InGameController.cs
+++ Assets/Scripts/GameSystem/InGameController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-    
+
 
 /****** 인게임의 모든 동작 관리 ******/
 public class InGameController : MonoBehaviour
@@ -17,10 +17,11 @@ public class InGameController : MonoBehaviour
 
     private int alivePlayers;
 
-    // TODO : 혹시 부활 기능 구현할 거라면 사용해야 할 필드
+    // TODO : 혹시 부활 기능 구현할 거라면 사용할 필드
+    // player에서 가지고 있는게 나을듯
     private bool isPlayer1Alive;
     private bool isPlayer2Alive;
-    
+
 
     // 갱신할 UI들
     // 인스펙터에서 연결 해야함
@@ -34,7 +35,7 @@ public class InGameController : MonoBehaviour
     public float GetScore() { return currentTime; }
     public float GetHighScore() { return highScore; }
 
-    
+
 
 
     // 게임 초기화
@@ -42,19 +43,18 @@ public class InGameController : MonoBehaviour
     {
         //게임 초기화
         currentTime = 0;
-
+        LoadHighScore();
 
         // UI 연결
         currentScoreText.text = currentTime.ToString("N3");
         highScoreText.text = highScore.ToString("N3");
 
-        if(GameManager.Instance.CurrentGameState == GameState.GameStart)
+        if (GameManager.Instance.CurrentGameState == GameState.GameStart)
         {
             InGameStart();
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (GameManager.Instance.CurrentGameState == GameState.GameStart)
@@ -65,10 +65,10 @@ public class InGameController : MonoBehaviour
     }
 
 
-    /** IntroState : 게임의 모든 상태를 초기화하여 새 게임 시작 가능토록 **/
+
     void CreatePlayer()
     {
-        Vector3 player1StartPosition = new Vector3 (0f, -4.2f, 0);
+        Vector3 player1StartPosition = new Vector3(0f, -4.2f, 0);
         Vector3 player2StartPosition = new Vector3(1.4f, -4.2f, 0);
         player1 = Instantiate(GameManager.Instance.player1Prefab, player1StartPosition, Quaternion.identity);
         alivePlayers = 1;
@@ -80,24 +80,12 @@ public class InGameController : MonoBehaviour
     }
 
 
-    /** SelectCharacterState : 캐릭터 선택 **/
-    /** SelectDifficultyState : 난이도 선택 **/
-    // 위 두 state는 GameManger에서 관리
-
-
-    /** GameStartState : 게임 시작 후 로직 **/
-
     internal void InGameStart()
     {
         // 플레이어 캐릭터 생성
         CreatePlayer();
 
         Time.timeScale = 1;
-
-        // TODO : 적 생성은 StageManager에서 수행!
-        // StageManager.Instance.~(); <- 필요 없음
-        // 다만 나중에 문제 생기면 GameState.GameStart 인 경우에만 생성되도록 하면 될듯
-
     }
 
 
@@ -115,6 +103,7 @@ public class InGameController : MonoBehaviour
             highScore = currentTime;
             highScoreText.text = highScore.ToString("N3");
         }
+        SaveHighScore();
     }
 
 
@@ -124,9 +113,36 @@ public class InGameController : MonoBehaviour
         // TODO : 부활 아이템 등을 넣을거라면 1P 2P 누가 살아있는지 체크도 해야 함
         alivePlayers--;
 
-        if(alivePlayers == 0)
+        if (alivePlayers == 0)
         {
             GameManager.Instance.GameOver();
         }
+    }
+
+
+    public void LoadHighScore()
+    {
+        if (PlayerPrefs.HasKey("highScore"))
+        {
+            float savedHighScore = PlayerPrefs.GetFloat("highScore");
+            if (savedHighScore > highScore)
+            {
+                highScore = savedHighScore;
+            }
+        }
+    }
+
+    public void SaveHighScore()
+    {
+        if (PlayerPrefs.HasKey("highScore"))
+        {
+            float savedHighScore = PlayerPrefs.GetFloat("highScore");
+            if (savedHighScore > highScore)
+            {
+                return;
+            }
+        }
+
+        PlayerPrefs.SetFloat("highScore", highScore);
     }
 }
