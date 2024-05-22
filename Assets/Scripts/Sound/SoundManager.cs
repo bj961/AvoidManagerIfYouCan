834ae51f7fdee608bcaa0cfe25dd506fc2caf0f1
bj961 <73685098+bj961@users.noreply.github.com>
2024-05-22 21:29:12 +0900

    Revert "Merge branch 'KSW+Dev' into MBJ_tempDev1"
    This reverts commit a809c0d0760cdb15a51f60a7f877370f4bef0002, reversing
    changes made to 77486176a308c83cdd543e3564a2615b67095bbe.

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -23,12 +23,13 @@ public class SoundManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        audioSource = GetComponent<AudioSource>();
     }
 
     void Start()
     {
-
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = introBGM;
+        audioSource.Play();
     }
 
     public void ChangeBGM(AudioClip audioClip)
